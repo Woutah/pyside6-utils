cd31f490cf147d11363bcc92128a8f20107a0d20
Wouter <wouterstokman@hotmail.com>
2023-03-05 20:09:01 +0100

    Example window

---
+++ Examples/run_all_widget_window.py
@@ -0,0 +1,28 @@
+
+
+from PySide6.QtWidgets import QApplication, QMainWindow
+from allWidgets_ui import Ui_MainWindow
+import pandas as pd
+from Models.PandasTableModel import PandasTableModel
+
+if __name__ == "__main__":
+	import sys
+	app = QApplication(sys.argv)
+	MainWindow = QMainWindow()
+	ui = Ui_MainWindow()
+	ui.setupUi(MainWindow)
+
+	#====== Example df for PandasTableView ======
+	example_df = pd.DataFrame({
+		"Column 1": [1, 2, 3, 4, 5],
+		"Column 2": [10, 20, 30, 40, 50],
+		"Column 3": [100, 200, 300, 400, 500],
+		"Column 4": [1000, 2000, 3000, 4000, 5000],
+		"Column 5": [10000, 20000, 30000, 40000, 50000],
+	})
+	example_df_model = PandasTableModel(example_df)
+	ui.pandasTableView.setModel(example_df_model)
+	ui.pandasTableView.setStatusBar(MainWindow.statusBar())
+
+	MainWindow.show()
+	sys.exit(app.exec())
