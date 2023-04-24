6b3cf16f688b8ce39597d804655d73501ae53862
Wouter <wouterstokman@hotmail.com>
2023-04-24 18:46:35 +0200

    Added proxymodel in-between

--- Widgets/PandasTableView.py
+++ Widgets/PandasTableView.py
@@ -1,16 +1,51 @@
 
 
 
-from PySide6.QtWidgets import QTableView, QApplication
+from PySide6.QtWidgets import QTableView, QApplication, QHeaderView
 from PySide6.QtCore import Qt
-import PySide6.QtCore as QtCore
+# import PySide6.QtCore as QtCore
+from PySide6 import QtCore, QtWidgets, QtGui
 from PySide6.QtGui import QKeySequence, QShortcut
+import typing
 import os
 import pandas as pd
+from enum import Enum
+
+class TableViewRoles(Enum):
+	headerRole = Qt.UserRole + 1
+
+# class FilterSortHeaderView(QHeaderView):
+# 	def __init__(self, orientation: QtCore.Qt.Orientation, parent: typing.Optional[QtWidgets.QWidget] = None) -> None:
+# 		super().__init__(orientation, parent)
+
+class PandasTableProxyModel(QtCore.QSortFilterProxyModel): #Enables sorting and filtering and special icons indicating which columns are sorted
+	def __init__(self, parent=None):
+		super().__init__(parent)
+
+		self._sort_columns = [] #List of column names to sort by
+		self._sort_order = [] #Qt.DescendingOrder or Qt.AscendingOrder
+		self._filter_columns = [] #List of column names to filter by
+		self._filter_strings = [] #List of strings to filter by
+
+	
+	def headerData(self, section: int, orientation: QtCore.Qt.Orientation, role: int = None) -> any:
+		if role == TableViewRoles.headerRole.value:
+			default_data = self.sourceModel().headerData(section, orientation, Qt.DisplayRole)
+			sort_by = None #None=not sorted, Qt.AscendingOrder=ascending, Qt.DescendingOrder=descending
+			#Check if this column is sorted
+			for i, col in enumerate(self._sort_columns):
+				if col == default_data:
+					sort_by = self._sort_order[i]
+					break
+
+			return (*default_data, )
+
+		return super().headerData(section, orientation, role)
+
+
 
 class PandasTableView(QTableView):
-	#Create a constant (uneditable) class-variable that stores a description of the widget
-	DESCRIPTION = "A view to display a pandas dataframe, works best in combination with PandasTableModel"
+	DESCRIPTION = "A view to display a pandas dataframe, works best in combination with PandasTableModel - places a proxymodel in between the tableview and the model to allow sorting and filtering"
 
 	def __init__(self, parent=None, status_bar=None):
 		QTableView.__init__(self, parent)
@@ -18,14 +53,37 @@ class PandasTableView(QTableView):
 		#If ctrl+c is pressed, copy the selection to the clipboard
 		self._copy_shortcut = QShortcut(QKeySequence("Ctrl+C"), self)
 		self._copy_shortcut.activated.connect(self.copySelection)
-	
+
+
+		self._proxy_model = PandasTableProxyModel(self)
+		self._proxy_model.setDynamicSortFilter(True)
+		self._proxy_model.setSourceModel(None)
+		super().setModel(self._proxy_model)
+
+		self.selectionModel().selectionChanged.connect(self.displaySelectionStats) #TODO: 
+
+		self.setSortingEnabled(True)
+		#Detect right-clicks on table headers
+	# 	self.horizontalHeader().sectionClicked.connect(self.headerClicked)
+		
+	# def headerClicked(self, logicalIndex):
+	# 	print("Header clicked", logicalIndex)
+	# 	print("Sorting by column", self._proxy_model.mapToSource(self._proxy_model.index(0, logicalIndex)).column())
+	# 	self._proxy_model.sort(self._proxy_model.mapToSource(self._proxy_model.index(0, logicalIndex)).column(), Qt.AscendingOrder)
+
+
 	def setStatusBar(self, status_bar):
 		self._status_bar = status_bar
 
 	def setModel(self, model: QtCore.QAbstractItemModel) -> None:
-		ret = super().setModel(model)
-		self.selectionModel().selectionChanged.connect(self.displaySelectionStats)
-		return ret
+		return self._proxy_model.setSourceModel(model)
+		#TODO: do we need a selection model?
+
+		#OLD (not using proxy model):
+		# ret = super().setModel(model)
+		# self.selectionModel().selectionChanged.connect(self.displaySelectionStats)
+		# return ret
+	
 	
 
 	
