397d3e195d747a7691cc4102a0b65e217840c3a2
Wouter <wouterstokman@hotmail.com>
2023-06-22 01:26:45 +0200

    Update gitignore - added __init__ files

---
+++ PySide6Wdgets/models/__init__.py
@@ -0,0 +1,6 @@
+
+from .dataclass_model import DataclassModel
+from .dataclass_tree_item import DataclassTreeItem
+from .extended_sort_filter_proxy_model import ExtendedSortFilterProxyModel
+from .file_explorer_model import FileExplorerModel
+from .pandas_table_model import PandasTableModel
---
+++ PySide6Wdgets/widgets/__init__.py
@@ -0,0 +1,11 @@
+"""Make all widgets importable using <package>.widgets.<widget>"""
+from .collapsible_group_box import CollapsibleGroupBox
+from .console_widget import ConsoleWidget
+from .dataclass_tree_view import DataClassTreeView
+from .extended_mdi_area import ExtendedMdiArea
+from .file_explorer_view import FileExplorerView
+from .frameless_mdi_window import FramelessMdiWindow
+from .overlay_widget import OverlayWidget
+from .pandas_table_view import PandasTableView
+from .range_selector import RangeSelector
+from .square_frame import SquareFrame
