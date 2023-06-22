2c04a7f72c58a1f739e5a14da3333cfc3d9ce7ba
Wouter <wouterstokman@hotmail.com>
2023-06-22 14:29:53 +0200

    Cleanup code and added console widget image

--- pyside6_utils/widgets/dataclass_tree_view.py
+++ pyside6_utils/widgets/dataclass_tree_view.py
@@ -88,3 +88,79 @@ class DataClassTreeView(QtWidgets.QTreeView):
 			return
 		model = self.model()
 		model.setData(index, default_val, QtCore.Qt.ItemDataRole.EditRole)
+
+
+
+
+def run_example_app():
+	"""Creates a qt-app instance and runs the example
+	Creates a temp file and mirrors the output to the "console", then deletes the temp file afterwards
+	"""
+	#pylint: disable=import-outside-toplevel
+	from pyside6_utils.models.console_widget_models.console_from_file_item import ConsoleFromFileItem
+	import tempfile
+	import threading
+	import time
+	log.info("Now running an example using console from file items, the console should print a number every second")
+	temp_dir = tempfile.gettempdir()
+	temp_file = tempfile.NamedTemporaryFile(dir=temp_dir, mode='w', delete=False, suffix=".txt") #Delete temporary
+		# file afterwards
+
+	app = QtWidgets.QApplication([])
+	test_console_model = ConsoleModel()
+	console_widget = ConsoleWidget()
+	console_widget.set_model(test_console_model)
+
+
+	test_console_model.add_item(
+		ConsoleFromFileItem(
+			name="Output 1",
+			path = temp_file.name,
+		)
+	)
+	test_console_model.add_item(
+		ConsoleFromFileItem(
+			name="Output 2",
+			path = temp_file.name,
+		)
+	)
+	window = QtWidgets.QMainWindow()
+	#Set size to 1000
+	window.resize(1200, 500)
+	console_widget.set_console_width_percentage(80)
+
+	layout = QtWidgets.QVBoxLayout()
+	layout.addWidget(console_widget)
+
+	console_widget.set_console_width_percentage(80)
+
+	#Create thread that
+	def log_to_file():
+		"""logs integer to file every seconds for 10 seconds"""
+		for i in range(20):
+			temp_file.write(f"Wrote line {i} to file {temp_file.name}\n")
+			temp_file.flush()
+			time.sleep(1)
+	thread = threading.Thread(target=log_to_file)
+	thread.start()
+
+
+	window.setCentralWidget(console_widget)
+	window.show()
+	app.exec()
+	temp_file.close()
+	#remove the temp file
+	os.remove(temp_file.name)
+
+
+
+if __name__ == "__main__":
+	formatter = logging.Formatter("[{pathname:>90s}:{lineno:<4}]  {levelname:<7s}   {message}", style='{')
+	handler = logging.StreamHandler()
+	handler.setFormatter(formatter)
+	logging.basicConfig(
+		handlers=[handler],
+		level=logging.DEBUG) #Without time
+
+	#Run example
+	run_example_app()
